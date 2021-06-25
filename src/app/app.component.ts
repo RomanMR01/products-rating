956c015430e159e285de1d3c3ad8305fe9860163
Roman Malchyshyn <roman_malchyshyn@epam.com>
2021-06-25 18:53:24 +0300

    Add optimizely integration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { environment } from './../environments/environment';
+import {environment} from './../environments/environment';
+import * as optimizelySDK from '@optimizely/optimizely-sdk';
 
 class Toggle {
   code: string;
@@ -12,21 +13,31 @@ class Toggle {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'products-rating';
   toggleName = 'stars.rating';
   stateON = 'ON';
   ratingEnabled = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
     if (environment.webService) {
-    const toggle = this.http.get('http://localhost:8081/feature-toggle/v1/' + this.toggleName);
-    toggle.subscribe((data: Toggle) => {
-      console.log('Response:' + JSON.stringify(data));
-      this.ratingEnabled = (this.stateON === data.state.valueOf());
-    });
-  }
+      const toggle = this.http.get('http://localhost:8081/feature-toggle/v1/' + this.toggleName);
+      toggle.subscribe((data: Toggle) => {
+        console.log('Response:' + JSON.stringify(data));
+        this.ratingEnabled = (this.stateON === data.state.valueOf());
+      });
+    } else if (environment.optimizely) {
+      const optimizelyClientInstance = optimizelySDK.createInstance({
+        sdkKey: '<key>'
+      });
+      optimizelyClientInstance.onReady().then(() => {
+        const enabled = optimizelyClientInstance.isFeatureEnabled('stars_rating', 'testU');
+        console.log('Is enabled:' + enabled);
+        this.ratingEnabled = enabled;
+      });
+    }
   }
 }
